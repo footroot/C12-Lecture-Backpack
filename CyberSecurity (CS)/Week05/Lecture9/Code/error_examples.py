34ec7256c26e53b72d925605aaa3360b72042f37
ArmandleRouxx <armandlr@hyperiondev.com>
2025-01-06 23:15:49 +0200

    Added exmaples for debugging session

---
+++ CyberSecurity (CS)/Week05/Lecture9/Code/error_examples.py
@@ -0,0 +1,57 @@
+# Syntax Errors
+
+# print "Hello world!"
+
+# for i in range(5)
+#     print(i)
+
+# num = 10
+# if num > 5:
+#     print(num)
+
+
+# Runtime
+
+# print("Program is running.")
+# x = 10/0
+
+# number = input("Please enter a number: ")
+# result = number + 10
+# print(result)
+
+# new_range = range("a", "d")
+
+
+# # Logical Error
+# interest = 0.05
+# total = 100 * interest
+# total = total / 100
+# print(total)
+
+# Loop to range 10
+# for i in range(0, 11):
+#     print(i)
+
+# # Incorrect calculation
+avg = (30 + 40 + 35 + 37) / 4
+print(avg)
+
+                                                        # On break
+# # Students Challenge
+print("Hello world")
+
+# class = "A"
+
+# my_string = "Hello"
+# print(my_string[5])
+
+x = 5
+if x == 10:
+    print(x)
+
+num1 = 5
+num2 = 10
+num3 = 4
+
+if num1 < num2 and num1 < num3:
+    print("Num1 is the smallest.")
